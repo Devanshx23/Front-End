e5347fbccf00f7b34b781120ee9ef7e86809c7cc
Devanshx23 <devanshsharma2925@gmail.com>
2025-01-28 23:19:44 +0530

    Homepage

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,13 @@
+import './asset/css/App.css';
+import Homepage from './components/Homepage';
+import Header from './components/Header';
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
